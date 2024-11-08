c90d8d669b5c727f4438f76a343ab1fa308e14ea
Riniel <rinielrodrigo555@gmail.com>
2024-11-08 13:48:57 -0300

    Remove yarn.lock file to use npm

--- db.js
+++ db.js
@@ -1,29 +1,14 @@
-// import mysql from "mysql";
-
-// export const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "riniel9255",
-//   database: "taskly",
-// });
 
 import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-// });
-
 export const db = mysql.createConnection({
-  host: 'autorack.proxy.rlwy.net',
-  user: 'root',
-  password: 'BTTDOCzrVxzBDIKJFqPskqFTQPKpBsIf',
-  database: 'railway',
-  port: 30948
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
